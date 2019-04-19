7169f41bd988494d1e56c7a7752389e5e64c794a
pexcn <i@pexcn.me>
2019-04-19 16:35:31 +0800

    shell: add tree package

--- shell/debian/9/02-apt-install.sh
+++ shell/debian/9/02-apt-install.sh
@@ -4,6 +4,6 @@ apt-get update
 apt-get install --no-install-recommends -y \
   bash-completion dialog ca-certificates psmisc net-tools \
   mtr-tiny traceroute gcc make automake autoconf libtool \
-  git nano curl
+  git nano tree curl
 dpkg -i packages/chkconfig*.deb
 apt-get clean
