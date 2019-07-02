06006fe220e986fad48d69d4060685545a5263ae
pexcn <i@pexcn.me>
2019-07-02 16:49:25 +0800

    vps: update cleaner

--- vps/debian/stretch/08-cleaner.sh
+++ vps/debian/stretch/08-cleaner.sh
@@ -4,6 +4,7 @@ cat << EOF > /usr/local/bin/cleaner
 #!/bin/bash
 cd /usr/share/doc && rm -rf *
 cd /var/lib/dpkg && rm -f *-old
+cd /var/cache/debconf && rm -f *-old
 cd /var/lib/apt/lists && rm -f * 2>/dev/null
 cd /var/cache/apt && rm -f * 2>/dev/null
 cd /var/cache/apt/archives && rm -f * 2>/dev/null
