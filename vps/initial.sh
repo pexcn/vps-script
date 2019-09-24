4d76faa6afed9e39e6f357c79cedb13ac698b6cb
pexcn <i@pexcn.me>
2019-09-24 17:33:53 +0800

    direct execute scripts

--- vps/initial.sh
+++ vps/initial.sh
@@ -6,15 +6,15 @@
 
 case $(cat /etc/debian_version) in
   9*)
-    bash debian/stretch/01-sources-list.sh
-    bash debian/stretch/02-apt-install.sh
-    bash debian/stretch/03-apt-purge.sh
-    bash debian/stretch/04-bashrc.sh
-    bash debian/stretch/05-git-config.sh
-    bash debian/stretch/06-rc-local.sh
-    bash debian/stretch/07-sysctl.sh
-    bash debian/stretch/08-cleaner.sh
-    bash debian/stretch/99-misc.sh
+    ./debian/stretch/01-sources-list.sh
+    ./debian/stretch/02-apt-install.sh
+    ./debian/stretch/03-apt-purge.sh
+    ./debian/stretch/04-bashrc.sh
+    ./debian/stretch/05-git-config.sh
+    ./debian/stretch/06-rc-local.sh
+    ./debian/stretch/07-sysctl.sh
+    ./debian/stretch/08-cleaner.sh
+    ./debian/stretch/99-misc.sh
     ;;
   *)
 	echo "Not supported."
