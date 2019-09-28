6a48d7619c98218b6bc6802de7598fe85e79875f
pexcn <i@pexcn.me>
2019-09-28 18:33:42 +0800

    update misc scripts

--- vps/debian/buster/99-misc.sh
+++ vps/debian/buster/99-misc.sh
@@ -8,5 +8,6 @@ dpkg-reconfigure -f noninteractive tzdata
 sed -i "s/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/g" /etc/gai.conf
 
 # disable boot entries
-systemctl disable getty@.service
 #systemctl list-unit-files | grep enabled
+systemctl disable getty@.service
+systemctl disable dbus-org.freedesktop.timesync1.service
--- vps/debian/stretch/99-misc.sh
+++ vps/debian/stretch/99-misc.sh
@@ -8,5 +8,5 @@ dpkg-reconfigure -f noninteractive tzdata
 sed -i "s/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/g" /etc/gai.conf
 
 # disable boot entries
-systemctl disable getty@.service
 #systemctl list-unit-files | grep enabled
+systemctl disable getty@.service
