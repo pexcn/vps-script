b4fa2ddd36ac497efa6546736c4a42b1c9621ae3
pexcn <i@pexcn.me>
2021-04-22 00:15:55 +0800

    debian: add limits script

---
+++ debian/common/01-limits.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# system limits
+mkdir /etc/security/limits.d
+cat << EOF > /etc/security/limits.d/90-limits.conf
+* soft nofile 1048576
+* hard nofile 1048576
+root soft nofile 1048576
+root hard nofile 1048576
+
+* soft nproc 51200
+* hard nproc 51200
+root soft nproc 51200
+root hard nproc 51200
+EOF
+
+# systemd limits
+cat << EOF >> /etc/systemd/system.conf
+DefaultLimitNOFILE=1048576
+DefaultLimitNPROC=51200
+EOF
+cat << EOF >> /etc/systemd/user.conf
+DefaultLimitNOFILE=1048576
+DefaultLimitNPROC=51200
+EOF
