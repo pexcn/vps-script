00ac96b6d72867b39fb2b904cc851a645851b400
pexcn <i@pexcn.me>
2021-05-15 14:12:41 +0800

    arch: update limits

--- archlinux/common/01-limits.sh
+++ archlinux/common/01-limits.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-mkdir /etc/security/limits.d/
-
 # system limits
+mkdir /etc/security/limits.d/
 cat << EOF > /etc/security/limits.d/90-limits.conf
 * soft nofile 1048576
 * hard nofile 1048576
