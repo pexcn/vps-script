6a9400ae755658401c2573d526ef722364cba70a
pexcn <i@pexcn.me>
2021-05-27 11:24:20 +0800

    arch-install: mirror tips

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -3,3 +3,4 @@
 wget https://raw.githubusercontent.com/pexcn/vps2arch/master/vps2arch
 chmod +x vps2arch
 ./vps2arch -b grub -n systemd-networkd
+#./vps2arch -b grub -n systemd-networkd -m "https://mirrors.163.com/archlinux"
