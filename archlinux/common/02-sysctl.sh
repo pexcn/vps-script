3dc9edb9bed4199e37049914eff7691eda31d430
pexcn <i@pexcn.me>
2021-06-08 16:47:52 +0800

    arch: update tcp kernel params

--- archlinux/common/02-sysctl.sh
+++ archlinux/common/02-sysctl.sh
@@ -16,16 +16,10 @@ net.ipv4.tcp_orphan_retries = 1
 net.ipv4.ip_local_port_range = 10001 65000
 net.core.netdev_max_backlog = 32768
 net.core.somaxconn = 4096
-
-# tcp fast open
 net.ipv4.tcp_fastopen = 3
 
-# tcp congestion control
-net.ipv4.tcp_congestion_control = bbr
-net.core.default_qdisc = fq_codel
-
 #
-# this values for low ram server (512MB) and high latency network (RTT 150ms+)
+# for 512m ram and rtt 150ms+ network
 #
 # 256MB 384MB 448MB
 net.ipv4.tcp_mem = 65536 98304 114688
@@ -35,4 +29,20 @@ net.core.rmem_max = 18750000
 net.core.wmem_max = 18750000
 net.core.rmem_default = 262144
 net.core.wmem_default = 262144
+net.ipv4.tcp_congestion_control = bbr
+net.core.default_qdisc = cake
+
+##
+## for 1024m ram and rtt 20ms+ network
+##
+## 512MB 768MB 896MB
+#net.ipv4.tcp_mem = 131072 196608 229376
+##net.ipv4.tcp_rmem = 4096 131072 6291456
+#net.ipv4.tcp_wmem = 4096 131072 4194304
+#net.core.rmem_max = 10000000
+#net.core.wmem_max = 10000000
+#net.core.rmem_default = 262144
+#net.core.wmem_default = 262144
+#net.ipv4.tcp_congestion_control = bbr
+#net.core.default_qdisc = fq_pie
 EOF
