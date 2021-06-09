ad5f58683ef4108fab15a502894368e80d009bdc
pexcn <i@pexcn.me>
2021-06-09 09:31:44 +0800

    arch-install: update tips

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/pexcn/vps2arch/master/vps2arch
 chmod +x vps2arch
 ./vps2arch -b grub -n systemd-networkd
 #./vps2arch -b grub -n systemd-networkd -m "https://mirrors.163.com/archlinux"
+#./vps2arch -b grub -n systemd-networkd -m "https://mirrors.kernel.org/archlinux"
