8632b4f226a641ad370fbddaade0e9151d355feb
pexcn <i@pexcn.me>
2021-06-25 20:44:51 +0800

    Update 03-locale.sh

--- debian/common/03-locale.sh
+++ debian/common/03-locale.sh
@@ -5,6 +5,6 @@ apt-get install locales --no-install-recommends -y
 sed -i '/^# en_US.UTF-8 UTF-8/s/^# //' /etc/locale.gen
 sed -i '/^# zh_CN.UTF-8 UTF-8/s/^# //' /etc/locale.gen
 sed -i '/^# zh_TW.UTF-8 UTF-8/s/^# //' /etc/locale.gen
-
 locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+echo LANG=en_US.UTF-8 > /etc/default/locale
