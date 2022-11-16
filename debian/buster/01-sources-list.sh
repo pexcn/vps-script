cc99d365aa26f5e9aaecc83aa5618c545bd5adc1
Sing Yu Chan <i@pexcn.me>
2022-11-16 10:48:54 +0800

    Update 01-sources-list.sh

--- debian/buster/01-sources-list.sh
+++ debian/buster/01-sources-list.sh
@@ -2,14 +2,7 @@
 
 cat << EOF > /etc/apt/sources.list
 deb http://deb.debian.org/debian buster main
-deb-src http://deb.debian.org/debian buster main
-
 deb http://deb.debian.org/debian buster-updates main
-deb-src http://deb.debian.org/debian buster-updates main
-
 deb http://security.debian.org/debian-security buster/updates main
-deb-src http://security.debian.org/debian-security buster/updates main
-
 deb http://deb.debian.org/debian buster-backports main
-deb-src http://deb.debian.org/debian buster-backports main
 EOF
