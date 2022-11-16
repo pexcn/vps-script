4289703ffd9c2f44822ce958737455847f1460af
Sing Yu Chan <i@pexcn.me>
2022-11-16 10:48:25 +0800

    Update 02-apt-install.sh

--- debian/buster/02-apt-install.sh
+++ debian/buster/02-apt-install.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 apt-get install --no-install-recommends -y \
-  bash-completion ca-certificates dialog psmisc net-tools mtr-tiny \
-  gcc make automake autoconf libtool git nano tree curl ethtool xz-utils
+  bash-completion ca-certificates dialog psmisc net-tools \
+  gcc make git nano tree curl ethtool xz-utils
 apt-get clean
